6b09a77a8a594a65ec897297cc6accad765df9d8
Jannik Kossen <jansen@clpc57.cs.ox.ac.uk>
2023-09-22 08:34:58 +0100

    [FEATURE] Add boostrap uncertainties from previous branch. (Not yet tested on new branch)

--- semantic_uncertainty/analyze_results.py
+++ semantic_uncertainty/analyze_results.py
@@ -1,5 +1,6 @@
 """Analyze uncertainty predictions."""
 import argparse
+import functools
 import logging
 import os
 import pickle
@@ -9,6 +10,9 @@ from sklearn.metrics import roc_curve, auc
 import wandb
 
 from uncertainty.utils import utils
+from uncertainty.utils.eval_utils import (
+    bootstrap, compatible_bootstrap, auroc, accuracy_at_quantile,
+    area_under_thresholded_accuracy)
 
 
 utils.setup_logger()
@@ -45,7 +49,9 @@ def init_wandb(wandb_runid, assign_new_wandb_id):
 
 def analyze_run(wandb_runid, assign_new_wandb_id=False, answer_fractions_mode='default'):
     '''Analyze the uncertainty measures for a given wandb run id.'''
+    logging.info('Analyzing wandb_runid `%s`.', wandb_runid)
 
+    # Set up evaluation metrics
     if answer_fractions_mode == 'default':
         answer_fractions = [0.8, 0.9, 0.95]
     elif answer_fractions_mode == 'finegrained':
@@ -53,7 +59,19 @@ def analyze_run(wandb_runid, assign_new_wandb_id=False, answer_fractions_mode='d
     else:
         raise ValueError
 
-    logging.info('Analyzing wandb_runid `%s`.', wandb_runid)
+    rng = np.random.default_rng(41)
+    eval_metrics = dict(zip(
+        ['AUROC', 'area_under_thresholded_accuracy', 'mean_uncertainty'],
+        list(zip(
+            [auroc, area_under_thresholded_accuracy, np.mean],
+            [compatible_bootstrap, compatible_bootstrap, bootstrap]
+        )),
+    ))
+    for answer_fraction in answer_fractions:
+        key = f'accuracy_at_{answer_fraction}_answer_fraction'
+        eval_metrics[key] = [
+            functools.partial(accuracy_at_quantile, quantile=answer_fraction),
+            compatible_bootstrap]
 
     if wandb.run is None:
         init_wandb(wandb_runid, assign_new_wandb_id=assign_new_wandb_id)
@@ -65,6 +83,7 @@ def analyze_run(wandb_runid, assign_new_wandb_id=False, answer_fractions_mode='d
         results_old = pickle.load(file)
 
     result_dict[wandb_runid] = {}
+
     # Iterate through the dictionary and compute additional metrics for each measure.
     for measure_name, measure_values in results_old['uncertainty_measures'].items():
         logging.info('Computing for uncertainty measure `%s`.', measure_name)
@@ -85,31 +104,28 @@ def analyze_run(wandb_runid, assign_new_wandb_id=False, answer_fractions_mode='d
             validation_is_falses.append(u_m[measure_name])
             logging_names.append(f'_max_from_{measure_name}')
 
+        # Iterate over predictions of 'falseness' or 'answerability'.
         for validation_is_false, logging_name in zip(validation_is_falses, logging_names):
             name = measure_name + logging_name
             result_dict[wandb_runid][name] = {}
 
-            # Can we use certainty (measure values) to predict if the model is false (validation_is_false)?
-            # (Higher measure_values should be predictive of with validation_is_false.)
-            fpr, tpr, _ = roc_curve(validation_is_false, measure_values)
-            auroc = auc(fpr, tpr)
+            validation_is_false = np.array(validation_is_false)
+            validation_accuracy = 1 - validation_is_false
 
-            result_dict[wandb_runid][name]['AUROC'] = auroc
+            fargs = {
+                'AUROC': [validation_is_false, measure_values],
+                'area_under_thresholded_accuracy': [validation_accuracy, measure_values],
+                'mean_uncertainty': [measure_values]}
 
-            logging.info("AUROC for measure name `%s`: %f", name, auroc)
-
-            # Compute 95% quantile of predictive entropy.
             for answer_fraction in answer_fractions:
+                fargs[f'accuracy_at_{answer_fraction}_answer_fraction'] = [validation_accuracy, measure_values]
 
-                # Get mask of indices where predictive entropy is below 95% quantile.
-                mask = np.array(measure_values) < np.quantile(
-                    measure_values, answer_fraction)
-
-                # Compute the mean of the values of validation_is_false where mask is True.
-                accuracy = 1 - np.mean(np.array(validation_is_false)[mask])
-
-                result_dict[wandb_runid][name][
-                    f'accuracy_at_{answer_fraction:.2f}_answer_fraction'] = accuracy
+            for fname, (function, bs_function) in eval_metrics.items():
+                metric_i = function(*fargs[fname])
+                result_dict[wandb_runid][name][fname] = metric_i
+                logging.info("%s for measure name `%s`: %f", fname, name, metric_i)
+                result_dict[wandb_runid][name][f'{fname}_bootstrap'] = bs_function(
+                    function, rng)(*fargs[fname])
 
     wandb.log(result_dict[wandb_runid])
     logging.info(
